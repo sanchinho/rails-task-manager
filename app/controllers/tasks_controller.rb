813775ec778aa022a7c5c937269071163a88c9a5
yannicksanchez <yannick.sanchez.guasch@gmail.com>
2018-08-07 19:15:49 +0200

    exercise w/ CRUD almost ended

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,15 +5,34 @@ class TasksController < ApplicationController
   end
 
   def new
-    @taks = Task.new
+    @task = Task.new
   end
 
   def show
     @task = Task.find(params[:id])
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
   def create
     @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
 
   private
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # index
+  get 'tasks', to: 'tasks#index'
+  # new
+  get 'tasks/new', to: 'tasks#new', as: :new
+  # show
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # create
+  post 'tasks', to: 'tasks#create', as: :create
+  # update
+  patch 'tasks/:id', to: 'tasks#update', as: :update_task
+  # edit
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # destroy
+  delete 'tasks', to: 'tasks#destroy', as: :destroy
 end
